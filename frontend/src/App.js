579a55391f91464d40f2e90706e36414090a01da
ZwierzchowskiM <marcin.zwierzchowski1@gmail.com>
2024-10-06 10:53:41 +0200

    add WelcomePage component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AddProductForm from './components/AddProductForm';
 import ProductList from './components/ProductList';
-import ProductDetails from './components/ProductDetails'; // Import strony szczegółów produktu
-import theme from './theme/theme'; // Import stworzony motyw
+import ProductDetails from './components/ProductDetails';
+import WelcomePage from './components/WelcomePage'; // Import nowej strony startowej
+import theme from './theme/theme'; 
 import { ThemeProvider } from '@mui/material/styles';
 import TextField from '@mui/material/TextField';
 
@@ -18,36 +19,35 @@ function App() {
     return (
         <ThemeProvider theme={theme}>
             <Router>
-                <div className="App">
-                    <h1>Moja aplikacja cenowa</h1>
+                <Routes>
+                    {/* Strona startowa */}
+                    <Route path="/" element={<WelcomePage />} />
 
-                    {/* Użycie komponentu TextField z Material-UI */}
-                    <TextField
-                        label="ID użytkownika"
-                        variant="outlined"
-                        value={userId}
-                        onChange={(e) => setUserId(e.target.value)}
-                        required
-                        sx={{ marginBottom: 2 }}
+                    {/* Główna aplikacja */}
+                    <Route
+                        path="/main"
+                        element={
+                            <div className="App">
+                                <h1>Moja aplikacja cenowa</h1>
+                                <TextField
+                                    label="ID użytkownika"
+                                    variant="outlined"
+                                    value={userId}
+                                    onChange={(e) => setUserId(e.target.value)}
+                                    required
+                                    sx={{ marginBottom: 2 }}
+                                />
+
+                                {/* Komponenty aplikacji */}
+                                <AddProductForm onProductAdded={handleProductAdded} userId={userId} />
+                                <ProductList userId={userId} key={productsUpdated} />
+                            </div>
+                        }
                     />
 
-                    {/* Definicja tras */}
-                    <Routes>
-                        {/* Strona szczegółów produktu */}
-                        <Route path="/products/:productId" element={<ProductDetails />} />
-
-                        {/* Główna strona z listą produktów */}
-                        <Route
-                            path="/"
-                            element={
-                                <>
-                                    <AddProductForm onProductAdded={handleProductAdded} userId={userId} />
-                                    <ProductList userId={userId} key={productsUpdated} />
-                                </>
-                            }
-                        />
-                    </Routes>
-                </div>
+                    {/* Strona szczegółów produktu */}
+                    <Route path="/products/:productId" element={<ProductDetails />} />
+                </Routes>
             </Router>
         </ThemeProvider>
     );
